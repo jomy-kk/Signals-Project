dc8265b24e1bd51d0a343aee37e1242911ae2515
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2021-01-03 16:42:29 +0000

    Improves image quality

--- python/display.py
+++ python/display.py
@@ -11,14 +11,14 @@ def e2_multi_channel_subplots(signalA, signalB, labelA, labelB, duration, fileTo
     if verbose:
         print("Plotting both signals...")
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(16,8))
 
     plt.subplot(2,1,1)
     t = np.linspace(0, duration, len(signalA), endpoint=False)
     plt.plot(t, signalA, 'b-', linewidth=0.5)
     plt.title(labelA)
     plt.xlabel('Time (s)')
-    plt.xlim((1, 2))
+    plt.xlim((1, 1.02))
     plt.ylabel('Amplitude (V)')
 
     plt.subplot(2, 1, 2)
@@ -26,8 +26,9 @@ def e2_multi_channel_subplots(signalA, signalB, labelA, labelB, duration, fileTo
     plt.plot(t, signalB, 'b-', linewidth=0.5)
     plt.title(labelB)
     plt.xlabel('Time (s)')
-    plt.xlim((1, 2))
+    plt.xlim((1, 1.02))
     plt.ylabel('Amplitude (V)')
+    plt.subplots_adjust(hspace=0.4)
 
     if fileToSave is not None:
         fig.savefig('../plots/' + fileToSave + '_partial.png', bbox_inches='tight')
@@ -36,7 +37,7 @@ def e2_multi_channel_subplots(signalA, signalB, labelA, labelB, duration, fileTo
 
     plt.show()
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(16,8))
 
     plt.subplot(2, 1, 1)
     t = np.linspace(0, duration, len(signalA), endpoint=False)
@@ -53,6 +54,7 @@ def e2_multi_channel_subplots(signalA, signalB, labelA, labelB, duration, fileTo
     plt.xlabel('Time (s)')
     plt.xlim((0, duration))
     plt.ylabel('Amplitude (V)')
+    plt.subplots_adjust(hspace=0.5)
 
     if fileToSave is not None:
         fig.savefig('../plots/' + fileToSave + '_complete.png', bbox_inches='tight')
@@ -67,6 +69,7 @@ def ef_save_to_csv(signal, fileToSave, verbose=False):
     if verbose: print("Saved signal in csv file to", fileToSave)
 
 # Test
+'''
 with open("../pickle/AL1_mock.pickle", 'rb') as AL1:
     signal_al1 = pickle.load(AL1)
     AL1.close()
@@ -75,6 +78,6 @@ with open("../pickle/AL4_mock.pickle", 'rb') as AL4:
     signal_al4 = pickle.load(AL4)
     AL4.close()
 
-#e2_multi_channel_subplots(signal_al1, signal_al4, 'AL1', 'AL4', 12, 'ExB - E2(A0, A5)', verbose=True)
-
-ef_save_to_csv(signal_al1, 'teste', True)
+e2_multi_channel_subplots(signal_al1, signal_al4, 'A0', 'A5', 12, 'ExB - E2(A0, A5)', verbose=True)
+'''
+#ef_save_to_csv(signal_al1, 'teste', True)
