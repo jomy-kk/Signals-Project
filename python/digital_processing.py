bc8875d80085328a206d80edac6ada68ae97cf05
TiagoMimoso <tiago.mimoso98@gmail.com>
2021-01-03 11:16:25 +0000

    Missing the time of the filter and the check of the cut-off-frequency in the filter.
    Close #12

--- python/digital_processing.py
+++ python/digital_processing.py
@@ -5,68 +5,51 @@ from scipy.io import wavfile
 from scipy.signal import freqz, lfilter, ellip, ellipord, cheb1ord, cheby1,iirdesign,lfilter, resample
 import csv
 
-def DI_BR_IIRchebyI(cs_file, plot_interval, verbose=False):
-    # ------------------------------------------------
-    # Create a signal.
-    # ------------------------------------------------
-
-    #Opening the pickle file
-    with open("caminho para CS......", "rb") as input_file:
+def DI_BR_IIRchebyI(file_name, plot_interval, verbose=False):
+    # --------------------------------------------------
+    # Opening the pickle file
+    # --------------------------------------------------
+    with open("../"+ file_name + ".pickle", "rb") as input_file:
          signal = cPickle.load(input_file)
-    nsamples = x.size
+         input_file.close()
+
+    # --------------------------------------------------
+    # Calculating parameters to use in the filter
+    # --------------------------------------------------
+    nsamples = len(signal)
+    time =   #time of the signal aquisition to then use to calculate the sample rate
+    samplerate = len(signal)/time
     t = arange(nsamples) / samplerate
 
     # Values and constants for the purpose of the C part of the project
     cut_off_frequency=1600 # Hz
-    Atenuation=60
+    atenuation=60
 
 
     # ------------------------------------------------
-    # Create a IIR filter and apply it to x.
+    # Create a IIR filter to apply it to x.
     # ------------------------------------------------
     N, Wn = cheb1ord(ws=[], wp= [0.5, 0.7], gstop=atenuation, gpass=1)
     b, a = cheby1(N, 1, Wn, btype='bandstop')
-    w, h = freqz(b, a, fs=200000)
+    w, h = freqz(b, a)
 
-    fig = plt.figure()
-    h_dB = 20 * np.log10(np.abs(h))
-    plt.plot(w, h_dB, 'b-')
-    #plt.ylim(-150, 5)
-    plt.ylabel('Magnitude (dB)')
-    plt.xlabel('Frequency (Hz)')
-    plt.grid(True)
-    fig.savefig('results_a6/chebyI_frequency_response.png', bbox_inches='tight')
+    h_dB = 20 * np.log10(np.abs(h)) # Log scale to use in the frequency response
+    # -----------------------------------------------
+    #  Apply the filter to the signal from the input file
+    #-----------------------------------------------
 
+    filtered_signal = lfilter(b, a, signal)
 
-    # Apply filtfilt to signal
-    filtered_x = lfilter(b, a, x)
-    wavfile.write('looneytunes_IIR_BR_55-65K.filtered_wav', samplerate, filtered_x)
-
-    fig = plt.figure()
-    # Plot the original signal.
-    plt.subplot(2, 1, 1)
-    plt.title('Original')
-    plt.plot(t, x, 'b-', linewidth=0.5)
-    plt.xlabel('Time (s)')
-    plt.xlim(plot_interval)
-    plt.ylim(-20500, 20500)
-    plt.grid(True)
-    # Plot the filtered signal, shifted to compensate for the phase delay.
-    plt.subplot(2, 1, 2)
-    plt.title('Filtered')
-    plt.plot(t, filtered_x, 'g', linewidth=0.5)
-    plt.xlim(plot_interval)
-    plt.ylim(-20500, 20500)
-    plt.xlabel('Time (s)')
-    plt.grid(True)
-    plt.subplots_adjust(hspace=0.7)
-    fig.savefig('results_a6/chebyI_filtered_output.png', bbox_inches='tight')
-
-    if verbose:
-        plt.show()
-
-DI_BR_IIRchebyI(signal1, (0,4), verbose=False)
+    return [w,h_dB,filtered_signal]
 
 
-def DR_uniformresample(pickle_file_path, plot_interval, verbose=False):
-    resample(cvs_file_path, 6, t=None, axis=0, window=None, domain='time')
+def DR_uniformresample(file_name, plot_interval, verbose=False):
+
+    with open("../"+ file_name + ".pickle", "rb") as uniform:
+         signal = cPickle.load(uniform)
+         uniform.close()
+
+    filtered_signal=resample(cvs_file_path, 3)
+
+
+    return (filtered_signal)
